b1ce466b2dca6458345b8516df5c70b03bc4be90
wildan taufik <89630704+wildantaufik354@users.noreply.github.com>
2023-05-17 12:19:21 +0700

    done!

--- app/Http/Controllers/AbsenController.php
+++ app/Http/Controllers/AbsenController.php
@@ -5,15 +5,15 @@ namespace App\Http\Controllers;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\URL;
 use RealRashid\SweetAlert\Facades\Alert;
-
 
 class AbsenController extends Controller
 {
   public function index()
   {
     $skema = DB::table('tb_skema')->get();
-
     return view('absen', compact('skema'));
   }
 
@@ -21,24 +21,18 @@ class AbsenController extends Controller
   {
     $absen_id = DB::table('tb_absen')->orderBy('id', 'desc')->first()->id + 1;
 
-    //signature
-
-    $folderPath = public_path('upload') . DIRECTORY_SEPARATOR;
-
+    // Signature
+    $folderUrl = URL::to('/upload');
     $image_parts = explode(";base64,", $request->signed);
     if (count($image_parts) < 2) {
       return back()->with('error', 'Invalid signature format');
     }
-
     $image_type_aux = explode("image/", $image_parts[0]);
-
     $image_type = isset($image_type_aux[1]) ? $image_type_aux[1] : null;
-
     $image_base64 = base64_decode($image_parts[1]);
+    $signature = uniqid() . '.' . $image_type;
 
-    $signature = $folderPath . uniqid() . '.' . $image_type;
-
-    file_put_contents($signature, $image_base64);
+    Storage::disk('public')->put('upload/' . $signature, $image_base64);
 
     DB::table('tb_absen')->insert([
       'nama' => $request->nama,
@@ -50,7 +44,7 @@ class AbsenController extends Controller
     DB::table('tb_signature')->insert([
       'absen_id' => $absen_id,
       'skema_absen' => $request->skema,
-      'tanda_tangan' => $signature
+      'tanda_tangan' => $folderUrl . '/' . $signature
     ]);
 
     return redirect()->back()->with('success', 'Data Absensi Anda Berhasil di Input.');
